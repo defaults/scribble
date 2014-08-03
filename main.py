2cda1f49b1e46a4438c6bae35952f0ca97485385
Vikash Kumar <mailkumarvikash@gmail.com>
2014-08-03 06:00:23 +0530

    updated app name

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         self.response.write(template.render())
 
 

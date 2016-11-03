a86c31bd6d0f23b6ec7b9249a0108fd6e2e278e7
Vikash Kumar <mailkumarvikash@gmail.com>
2016-11-03 22:39:30 +0530

    adding config file

--- config/config.py
+++ config/config.py
@@ -1,8 +1,24 @@
-project_name = 'My personal page'
-domain = 'https://vikashkumar.me'
+project_name = 'PROJECT_NAME'
+domain = ''
 
 admin = {
-    'admin_name': 'Vikash Kumar',
-    'admin_mail': 'mailkumarvikash@gmail.com',
-    'time_zone': 'Asia/Kollatta'
+    'admin_name': 'ADMIN_NAME',
+    'admin_mail': 'ADMIN_NAME',               # email registered to send email.
+    'admin_mobile': 'ADMIN_MOBILE_NO',
+    'time_zone': 'ADMIN_TIMEZONE'             # Admin timezone. default UTC.
+}
+
+# leave blank if fb account kit login not required
+fb_account_kit = {
+    'api_version': "v1.0"
+    'app_id': 'APP_ID'
+    'app_secret': 'APP_SECRET'
+    'me_endpoint_url': 'https://graph.accountkit.com/v1.0/me'
+    'token_exchange_url': 'https://graph.accountkit.com/v1.0/access_token'
+}
+
+application_config = {
+    'webapp2_extras.sessions' = {
+        'secret_key': 'VERY_SECRET_KEY',
+    }
 }

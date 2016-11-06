e6d3ff70d8aa39f4e76b76d4956e599372d1e281
Vikash Kumar <mailkumarvikash@gmail.com>
2016-11-06 23:39:32 +0530

    user model

--- models/model.py
+++ models/model.py
@@ -6,6 +6,12 @@ import string
 import datetime
 import types
 from datetime import datetime
+import time
+import webapp2_extras.appengine.auth.models
+
+from google.appengine.ext import ndb
+
+from webapp2_extras import security
 
 from google.appengine.api import images
 from google.appengine.ext import ndb
@@ -126,3 +132,31 @@ class Auth(ndb.Model, Jsonifiable):
     created_on = ndb.DateTimeProperty(auto_now_add=True)
     modified_on = ndb.DateTimeProperty()
     soft_deleted = ndb.BooleanProperty(default=False)
+
+
+# User model extending webapp2 auth expando user model
+class User(webapp2_extras.appengine.auth.models.User):
+    @classmethod
+    def get_by_auth_token(cls, user_id, token, subject='auth'):
+        """Returns a user object based on a user ID and token.
+        :param user_id:
+            The user_id of the requesting user.
+        :param token:
+            The token string to be verified.
+        :returns:
+            A tuple ``(User, timestamp)``, with a user object and
+            the token timestamp, or ``(None, None)`` if both were not found.
+        """
+        token_key = cls.token_model.get_key(user_id, subject, token)
+        user_key = ndb.Key(cls, user_id)
+        # Use get_multi() to save a RPC call.
+        valid_token, user = ndb.get_multi([token_key, user_key])
+        if valid_token and user:
+            timestamp = int(time.mktime(valid_token.created.timetuple()))
+            return user, timestamp
+
+        return None, None
+
+    @classmethod
+    def get_by_unique_propertiy():
+        pass

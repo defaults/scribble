5e858aeeeffc9e32a3aebddec52b7d30ebfb46be
Vikash Kumar <mailkumarvikash@gmail.com>
2016-12-31 18:20:42 +0530

    adding XSRF verification and other changes

--- controllers/authentication.py
+++ controllers/authentication.py
@@ -36,110 +36,7 @@ def authenticated(handler):
         return check_authentication
 
 
-class LoginServicesHandler(base_controller.BaseHandler):
-    """login handlar - handler all types of login."""
-
-    def accountkit_login(self, code, csrf):
-        """Initialtes fb accountkit mobile no based login
-
-        :params code:
-            code to verify login request
-        :params csrf:
-            csrf nonce
-        :returns:
-            logged in mobile_no
-        """
-        api_version = config.fb_account_kit['api_version']
-        app_id = config.fb_account_kit['app_id']
-        app_secret = config.fb_account_kit['app_secret']
-        me_endpoint_base_url = config.fb_account_kit['me_endpoint_url']
-        token_exchange_base_url = config.fb_account_kit['token_exchange_url']
-
-        if CSRFHandlar.verify_csrf(csrf):
-            app_access_token = '|'.join(['AA', app_id, app_secret])
-            params = {
-                'grant_type': 'authorization_code',
-                'code': code,
-                'access_token': app_access_token
-            }
-
-            token_exchange_url = token_exchange_base_url + '?' + \
-                urllib.urlencode(params)
-            token_exchange_req = urllib2.urlopen(token_exchange_url)
-            token_exchange_result = json.loads(token_exchange_req.read()
-                                               .decode('utf-8'))
-            me_endpoint_url = me_endpoint_base_url + '?access_token=' + \
-                token_exchange_result['access_token']
-            me_detail_request = urllib2.urlopen(me_endpoint_url)
-            me_detail_response = json.loads(me_detail_request
-                                            .read().decode('utf-8'))
-
-            return me_detail_response['phone']
-        else:
-            raise Exception('error', 'invalid CSRF token')
-
-    def initiate_email_login(self, user):
-        """Initialtes email based login by sending verification mail
-
-        :params user:
-            user
-        :returns:
-            status(boolean)
-        """
-        user_id = user.get_id()
-        token = self.user_model.create_signup_token(user_id)
-        verification_url = self.uri_for(
-            'verification', type='signup', user_id=user_id,
-            signup_token=token, _full=True)
-
-        valid_till = datetime.time.now()
-
-        email_payload = {
-            'verification_url': verification_url,
-            'valid_till': valid_till
-        }
-
-        self.send_email(user.email, email_type='signup', payload=email_payload)
-
-        return
-
-    def verify_user(self, user_id, signup_token, type):
-        “"""varifies user based on request type and email.
-            currently used for email login verification
-
-        :params user_id:
-            user Id of user
-        :params signup_token:
-            signup token
-        :params type:
-            verification type
-        :returns:
-            user if verified
-        """”
-        user, ts = self.user_model.get_by_auth_token(
-            int(user_id), signup_token, 'signup')
-
-        if not user:
-            logging.info(
-                'Could not find any user with id "%s" signup token "%s"',
-                user_id, signup_token)
-            raise Exception('error', 'Could not find any user')
-
-        # store user data in the session
-        self.auth.set_session(
-            self.auth.store.user_to_dict(user), remember=True)
-
-        if type == 'signup':
-            self.user_model.delete_signup_token(user.get_id(), signup_token)
-
-        if not user.verified:
-            user.verified = True
-            user.put()
-
-        return user
-
-
-class CSRFHandlar(LoginServicesHandler):
+class CSRFHandlar(base_controller.BaseHandler):
     """docstring for CSRFHandlar."""
 
     # String used instead of user id when there is no user.
@@ -242,7 +139,8 @@ class CSRFHandlar(LoginServicesHandler):
             user = ANONYMOUS_USER
             if users.get_current_user():
                 user = users.get_current_user().user_id()
-            if not validate_token(XsrfSecret.get(), token, user, path):
+            if not validate_token(config.CONFIG.CSRF_SECRET_KEY,
+                                  token, user, path):
                 self.error(403)
                 return
 
@@ -264,4 +162,105 @@ class CSRFHandlar(LoginServicesHandler):
             user = ANONYMOUS_USER
         if users.get_current_user():
             user = users.get_current_user().user_id()
-        return generate_token(XsrfSecret.get(), user, path)
+        return generate_token(config.CONFIG.CSRF_SECRET_KEY, user, path)
+
+
+class LoginServicesHandler(CSRFHandlar):
+    """login handlar - handler all types of login."""
+
+    def accountkit_login(self, code, csrf):
+        """Initialtes fb accountkit mobile no based login
+
+        :params code:
+            code to verify login request
+        :params csrf:
+            csrf nonce
+        :returns:
+            logged in mobile_no
+        """
+        api_version = config.CONFIG.fb_accountkit_api_version
+        app_id = config.CONFIG.fb_account_kit_app_id
+        app_secret = config.CONFIG.fb_account_kit_app_secret
+        me_endpoint_base_url = config.CONFIG.fb_account_kit_endpoint_url
+        token_exchange_base_url = \
+            config.CONFIG.fb_account_kit_token_exchange_url
+
+        app_access_token = '|'.join(['AA', app_id, app_secret])
+        params = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'access_token': app_access_token
+        }
+
+        token_exchange_url = token_exchange_base_url + '?' + \
+            urllib.urlencode(params)
+        token_exchange_req = urllib2.urlopen(token_exchange_url)
+        token_exchange_result = json.loads(token_exchange_req.read()
+                                           .decode('utf-8'))
+        me_endpoint_url = me_endpoint_base_url + '?access_token=' + \
+            token_exchange_result['access_token']
+        me_detail_request = urllib2.urlopen(me_endpoint_url)
+        me_detail_response = json.loads(me_detail_request
+                                        .read().decode('utf-8'))
+
+        return me_detail_response['phone']
+
+    def initiate_email_login(self, user):
+        """Initialtes email based login by sending verification mail
+
+        :params user:
+            user
+        :returns:
+            status(boolean)
+        """
+        user_id = user.get_id()
+        token = self.user_model.create_signup_token(user_id)
+        verification_url = self.uri_for(
+            'verification', type='signup', user_id=user_id,
+            signup_token=token, _full=True)
+
+        valid_till = datetime.time.now()
+
+        email_payload = {
+            'verification_url': verification_url,
+            'valid_till': valid_till
+        }
+
+        self.send_email(user.email, email_type='signup', payload=email_payload)
+
+        return
+
+    def verify_user(self, user_id, signup_token, type):
+        “"""varifies user based on request type and email.
+            currently used for email login verification
+
+        :params user_id:
+            user Id of user
+        :params signup_token:
+            signup token
+        :params type:
+            verification type
+        :returns:
+            user if verified
+        """”
+        user, ts = self.user_model.get_by_auth_token(
+            int(user_id), signup_token, 'signup')
+
+        if not user:
+            logging.info(
+                'Could not find any user with id "%s" signup token "%s"',
+                user_id, signup_token)
+            raise Exception('error', 'Could not find any user')
+
+        # store user data in the session
+        self.auth.set_session(
+            self.auth.store.user_to_dict(user), remember=True)
+
+        if type == 'signup':
+            self.user_model.delete_signup_token(user.get_id(), signup_token)
+
+        if not user.verified:
+            user.verified = True
+            user.put()
+
+        return user
